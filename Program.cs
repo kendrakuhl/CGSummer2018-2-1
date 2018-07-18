7f6d39e52d00dca31d53c4dd58051c8f46dbfabb
Kendra Kuhl <kendra@kuhl.ws>
2018-07-18 07:32:46 -0500

    Add Comments

--- Program.cs
+++ Program.cs
@@ -8,7 +8,10 @@ namespace CGSummer2018_2_1
         {
             // Prompt the user for their name:
             Console.Write("I'm hello world. What is your name? ");
+            // Get the username from the console and put it into a variable named userName
             var userName = Console.ReadLine();
+            // Write Hello userName to the console. String.Format is a nice way to concatenate strings 
+            // without having a bunch of + signs everywhere
             Console.WriteLine(string.Format("Hello {0}!", userName));
             Console.Read();
         }
