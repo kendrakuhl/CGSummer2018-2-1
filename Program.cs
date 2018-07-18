7aec91a6711068f9b2635c48fdf11325f73a63d7
Kendra Kuhl <kendra@kuhl.ws>
2018-07-18 07:30:17 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CGSummer2018_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user for their name:
+            Console.Write("I'm hello world. What is your name? ");
+            var userName = Console.ReadLine();
+            Console.WriteLine(string.Format("Hello {0}!", userName));
+            Console.Read();
+        }
+    }
+}
